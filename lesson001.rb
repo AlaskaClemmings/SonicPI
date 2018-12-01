5c4058c140a34e6bf9f6baf0c1d35f3c59e71caa
AlaskaClemmings <45451932+AlaskaClemmings@users.noreply.github.com>
2018-12-01 10:23:57 +0100

    Create lesson001.rb

---
+++ lesson001.rb
@@ -0,0 +1,31 @@
+sample :ambi_drone, rate: 0.5, attack: 0.75, release: 0.75
+play 36
+play 48
+sample :ambi_lunar_land, rate: 0.5
+sample :ambi_drone, rate: 0.5
+play 79
+sleep 0.5
+play 72
+sleep 0.5
+play 75
+sample :ambi_lunar_land, rate: 0.25
+sleep 1
+play 48
+sleep 0.5
+play 36
+sample :ambi_drone, rate: 0.25
+sleep 1
+play 36
+play 36
+play 48
+play 36
+play 48
+sample :ambi_lunar_land, rate: 0.5
+sample :ambi_drone, rate: 0.5
+play 36
+play 36
+play 48
+play 36
+play 48
+play :C
+play :E
